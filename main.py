b36d8a921fcec1f1d589b61c93019e503a5aa4ca
RationalNivesh <104433699+RationalNivesh@users.noreply.github.com>
2022-04-28 16:26:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#Objective-User should know how much the movie ticket costs
+#Movies-6
+#Learning- If,Elif,Else
+
+#Input
+choice=input("1.Sound of Music 2.Encanto 3.Top GUn 4.Rocky I 5.Rocky II 6.Turning Red")
+
+#Costs $$$
+if choice == "1":
+  print("This ticket costs $6")
+elif choice == "2":
+  print("This ticket costs $7")
+elif choice == "3":
+  print("This movie ticket costs $7")
+elif choice == "4":
+  print("This movie costs $10")
+elif choice == "5":
+  print("This movie costs $10")
+elif choice == "6":
+  print("This movie costs $8")
+else:
+  print("Wrong Input,Movie is not in the list")
